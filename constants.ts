fe8a60173335fc834e5d3a8061af6d743b63a537
Kevin Samosir <bintangkevin29@gmail.com>
2020-11-23 17:10:09 +0700

    added constant

---
+++ constants.ts
@@ -0,0 +1,9 @@
+export const CONSTANTS = {
+  primaryColor: '##FFBB3B',
+  backgroundColor: '#000000',
+  whiteColor: '#ffffff',
+  fontLight: 'GothamBook',
+  fontRegular: 'GothamLight',
+  fontMedium: 'GothamMedium',
+  fontBold: 'GothamBold',
+};

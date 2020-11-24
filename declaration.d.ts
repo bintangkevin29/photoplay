45a012c2c7c796a28ef98d179285357faa25c1ea
Kevin Samosir <bintangkevin29@gmail.com>
2020-11-24 14:25:20 +0700

    moved autocmplete type to definition

--- declaration.d.ts
+++ declaration.d.ts
@@ -4,19 +4,3 @@ declare module '*.svg' {
   const content: React.FC<SvgProps>;
   export default content;
 }
-
-export type AutoCompleteType =
-  | 'password'
-  | 'cc-csc'
-  | 'cc-exp'
-  | 'cc-exp-month'
-  | 'cc-exp-year'
-  | 'cc-number'
-  | 'email'
-  | 'name'
-  | 'postal-code'
-  | 'street-address'
-  | 'tel'
-  | 'username'
-  | 'off'
-  | undefined;
---
+++ definition.d.ts
@@ -0,0 +1,15 @@
+export type AutoCompleteType =
+  | 'password'
+  | 'cc-csc'
+  | 'cc-exp'
+  | 'cc-exp-month'
+  | 'cc-exp-year'
+  | 'cc-number'
+  | 'email'
+  | 'name'
+  | 'postal-code'
+  | 'street-address'
+  | 'tel'
+  | 'username'
+  | 'off'
+  | undefined;

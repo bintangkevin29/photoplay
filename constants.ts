42d5bd4281272d6d379a91b865edda3bc9bb6c76
Kevin Samosir <bintangkevin29@gmail.com>
2020-11-24 14:25:33 +0700

    added some new colors on constant

--- constants.ts
+++ constants.ts
@@ -1,9 +1,14 @@
 export const CONSTANTS = {
-  primaryColor: '##FFBB3B',
+  primaryColor: '#FFBB3B',
+  primaryGradientColor: '#FFA90A',
   backgroundColor: '#000000',
   whiteColor: '#ffffff',
-  fontLight: 'GothamBook',
-  fontRegular: 'GothamLight',
+  darkerGreyColor: '#212121',
+  greyColor: '#C6C6C6',
+  lighterGreyColor: '#A2A2A2',
+  fontRegular: 'GothamBook',
+  fontLight: 'GothamLight',
   fontMedium: 'GothamMedium',
   fontBold: 'GothamBold',
+  borderRadius: 9,
 };
